03c097f4e1b3abc45aa1f4955668cbc943953f13
patrickatcw <pj8392@yahoo.com>
2018-07-23 14:15:31 -0400

    added class

---
+++ src/main/java/com/JavaBookstore/JavaBookstore/service/impl/UserSecurityService.java
@@ -0,0 +1,29 @@
+package com.JavaBookstore.JavaBookstore.service.impl;
+
+import com.JavaBookstore.JavaBookstore.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.JavaBookstore.JavaBookstore.domain.User;
+
+@Service
+public class UserSecurityService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;  //will need to make UserRepository class
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        com.JavaBookstore.JavaBookstore.domain.User user = userRepository.findByUsername(username);
+
+        if(null == user) {
+            throw new UsernameNotFoundException("Username not found");
+        }
+
+        return user;
+    }
+
+}
