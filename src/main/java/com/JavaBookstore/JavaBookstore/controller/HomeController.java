6b0e02ac01242f2483d5699ed516c7cc5ba184f8
patrickatcw <pj8392@yahoo.com>
2018-07-23 14:16:14 -0400

    changes to homecontroller

--- src/main/java/com/JavaBookstore/JavaBookstore/controller/HomeController.java
+++ src/main/java/com/JavaBookstore/JavaBookstore/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.JavaBookstore.JavaBookstore.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,9 +12,29 @@ public class HomeController {
       return  "index";
     }
 
-    @RequestMapping("/myAccount")
+    /*@RequestMapping("/myAccount")
     public String myAccount(){
         return  "myAccount";
+    }*/
+
+    //added while working in securityconfig branch, and commented out /myAccount
+    //in conjunction with myAccount.html changes, which noted in comments
+    @RequestMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("classActiveLogin", true);
+        return "myAccount";
+    }
+
+    @RequestMapping("/forgetPassword")
+    public String forgetPassword(Model model) {
+        model.addAttribute("classActiveForgetPassword", true);
+        return "myAccount";
+    }
+
+    @RequestMapping("/newUser")
+    public String newUser(Model model) {
+        model.addAttribute("classActiveNewUser", true);
+        return "myAccount";
     }
 
 }
