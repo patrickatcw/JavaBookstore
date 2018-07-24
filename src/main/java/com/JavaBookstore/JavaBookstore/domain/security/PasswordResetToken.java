d5363eff81fcecf763e3fd2ebefb9326ce15d421
patrickatcw <pj8392@yahoo.com>
2018-07-24 16:32:52 -0400

    added this class

---
+++ src/main/java/com/JavaBookstore/JavaBookstore/domain/security/PasswordResetToken.java
@@ -0,0 +1,96 @@
+package com.JavaBookstore.JavaBookstore.domain.security;
+
+import com.JavaBookstore.JavaBookstore.domain.User;
+
+import javax.persistence.*;
+import java.util.Calendar;
+import java.util.Date;
+
+@Entity
+public class PasswordResetToken {
+
+    private static final int EXPIRATION = 60 * 24;
+
+    //fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String token;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable=false, name="user_id")
+    private User user;
+
+    private Date expiryDate;
+
+    //constructor
+    public PasswordResetToken(final String token, final User user) {
+        super ();
+
+        this.token = token;
+        this.user = user;
+        this.expiryDate = calculateExpiryDate(EXPIRATION);
+    }
+
+    //method
+    private Date calculateExpiryDate (final int expiryTimeInMinutes) {
+        final Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(new Date().getTime());
+        cal.add(Calendar.MINUTE, expiryTimeInMinutes);
+        return new Date(cal.getTime().getTime());
+    }
+
+    //method
+    public void updateToken(final String token) {
+        this.token = token;
+        this.expiryDate = calculateExpiryDate(EXPIRATION);
+    }
+
+    //getters and setters
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public static int getExpiration() {
+        return EXPIRATION;
+    }
+
+    //generated toString method using the fields
+    @Override
+    public String toString() {
+        return "PasswordResetToken [id=" + id + ", token=" + token + ", user=" + user + ", expiryDate=" + expiryDate
+                + "]";
+    }
+
+
+}
