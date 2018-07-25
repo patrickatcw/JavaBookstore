f27a4fc9b4dd56fd87967a546c729b649f5f97e2
patrickatcw <pj8392@yahoo.com>
2018-07-25 10:13:36 -0400

    added more to this file, most important is validation for user, need to add myProfile html file template next, do that in new branch

--- src/main/java/com/JavaBookstore/JavaBookstore/controller/HomeController.java
+++ src/main/java/com/JavaBookstore/JavaBookstore/controller/HomeController.java
@@ -1,8 +1,14 @@
 package com.JavaBookstore.JavaBookstore.controller;
 
+import com.JavaBookstore.JavaBookstore.domain.User;
 import com.JavaBookstore.JavaBookstore.domain.security.PasswordResetToken;
+import com.JavaBookstore.JavaBookstore.service.impl.UserSecurityService;
 import com.JavaBookstore.JavaBookstore.service.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +19,11 @@ import java.util.Locale;
 @Controller
 public class HomeController {
 
-    @Autowired UserService userService;
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserSecurityService userSecurityService;
 
     @RequestMapping("/")
     public String index(){
@@ -55,8 +65,36 @@ public class HomeController {
             @RequestParam("token") String token,
             Model model) {
         PasswordResetToken passToken = userService.getPasswordResetToken(token);//create new interface UserService
-        model.addAttribute("classActiveNewUser", true);
-        return "myAccount";
+
+        //authentication, code block below done while working in pswrdrstkn
+        //user can be found by a token
+        if (passToken == null) {
+            String message = "Invalid Token.";
+            model.addAttribute("message", message);
+            return "redirect:/badRequest";  //done when invalid input
+        }
+
+        User user = passToken.getUser();
+        String username = user.getUsername();
+
+        //sets current login session to that user, codeblock below
+
+        //user details are obtained by user security service
+        UserDetails userDetails = userSecurityService.loadUserByUsername(username);  //must be autowired
+
+        //authentication using user details
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(),
+                userDetails.getAuthorities());
+
+        //sets authentication to the current user
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        /*model.addAttribute("classActiveNewUser", true);
+        return "myAccount";*/
+        //change from above in pswrdrstkn branch need to add myProfile next
+        model.addAttribute("classActiveEdit", true);
+        return "myProfile"; //need to add myProfile html file template next
+
     }
 
 }
