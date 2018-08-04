bfe1f5c60d10a096bf9e7e931961dfebda9a881c
patrickatcw <pj8392@yahoo.com>
2018-08-04 10:15:37 -0400

    making some changes for layout, but i think backend up to this point is working as should, will continue to check and validate before committing branch

--- src/main/java/com/JavaBookstore/JavaBookstore/utility/MailConstructor.java
+++ src/main/java/com/JavaBookstore/JavaBookstore/utility/MailConstructor.java
@@ -24,7 +24,7 @@ public class MailConstructor {
         //had to change = to + to get link to work sent in initial user validation email
         //and in passwordresettoken had to add default oonstructor,  public PasswordResetToken(){}
         String url = contextPath + "/newUser?token="+token;
-        String message = "\nPlease click on this link to verify your email and edit your personal information. Your password is: \n"+password + "This" +
+        String message = "\nPlease click on this link to verify your email and edit your personal information. Your password is: \n"+password + " This " +
                 "password will expire in 24 hours, so don't diddle daddle!";
         SimpleMailMessage email = new SimpleMailMessage();
         email.setTo(user.getEmail());
