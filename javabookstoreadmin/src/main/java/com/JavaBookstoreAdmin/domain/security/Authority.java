2210a1068f4775d01984752c2ebb79ab3c7dc4a3
patrickatcw <pj8392@yahoo.com>
2018-08-29 19:16:39 -0400

    added admin portal, on a different server, not sure if i can put this within JavaBookstore file, see what happens???

---
+++ javabookstoreadmin/src/main/java/com/JavaBookstoreAdmin/domain/security/Authority.java
@@ -0,0 +1,22 @@
+package com.JavaBookstoreAdmin.domain.security;
+
+import org.springframework.security.core.GrantedAuthority;
+
+//implements GrantedAuthority
+public class Authority implements GrantedAuthority{
+
+    //fields
+    private final String authority;
+
+    //constuctor
+    public Authority(String authority) {
+        this.authority = authority;
+    }
+
+    //override method
+    @Override
+    public String getAuthority() {
+        //return null;
+        return authority;
+    }
+}
