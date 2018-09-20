4161cb9ecdb82951d9a71f09fdcbf2fe59a99e6c
patrickatcw <pj8392@yahoo.com>
2018-09-20 08:33:02 -0400

    just some minor display changes

--- src/main/java/com/JavaBookstore/JavaBookstore/config/SecurityConfig.java
+++ src/main/java/com/JavaBookstore/JavaBookstore/config/SecurityConfig.java
@@ -40,7 +40,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/login",
             "/fonts/**",
             "/bookshelf",
-            "/detailBook"
+            //"/detailBook"
+            "/detailBook/**",
 
     };
 
