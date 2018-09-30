30c6b1916903f8b674ae2fedb0cb1142e4e5b5fe
patrickatcw <pj8392@yahoo.com>
2018-09-30 09:38:17 -0400

    added this, now add the associated classes

---
+++ src/main/java/com/JavaBookstore/JavaBookstore/domain/ShoppingBasket.java
@@ -0,0 +1,60 @@
+package com.JavaBookstore.JavaBookstore.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+public class ShoppingBasket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private BigDecimal GrandTotal;
+
+    @OneToMany(mappedBy="shoppingBasket", cascade= CascadeType.ALL, fetch=FetchType.LAZY)
+    @JsonIgnore
+    private List<BasketItem> basketItemList;  //need to make basketitem class, done
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getGrandTotal() {
+        return GrandTotal;
+    }
+
+    public void setGrandTotal(BigDecimal grandTotal) {
+        GrandTotal = grandTotal;
+    }
+
+    public List<BasketItem> getCartItemList() {
+        return basketItemList;
+    }
+
+    public void setCartItemList(List<BasketItem> basketItemList) {
+        this.basketItemList = basketItemList;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+
+}
+
+
+
