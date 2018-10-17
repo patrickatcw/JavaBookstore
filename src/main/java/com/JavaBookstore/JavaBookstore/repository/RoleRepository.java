8cab7d539f6fd4a94d8da5c60325851adf5ff6c8
patrickatcw <pj8392@yahoo.com>
2018-10-17 15:35:35 -0400

    not adding book to basket just yet, missing something, some investigating to do...

--- src/main/java/com/JavaBookstore/JavaBookstore/repository/RoleRepository.java
+++ src/main/java/com/JavaBookstore/JavaBookstore/repository/RoleRepository.java
@@ -4,5 +4,7 @@ import com.JavaBookstore.JavaBookstore.domain.security.Role;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RoleRepository extends CrudRepository<Role, Long> {
-    Role findByname(String name);
+
+    Role findByName(String name);
+
 }
